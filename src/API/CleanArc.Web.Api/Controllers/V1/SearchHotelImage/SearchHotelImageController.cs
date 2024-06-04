9fa385c73dbaed27201d03ff9fe669281698340e
Waqas <aliw6065@gmail.com>
2024-06-04 16:30:54 +0500

    Searching Hotel

---
+++ src/API/CleanArc.Web.Api/Controllers/V1/SearchHotelImage/SearchHotelImageController.cs
@@ -0,0 +1,32 @@
+﻿using Asp.Versioning;
+using CleanArc.Application.Features.SearchHotel.Commands.CreateSearchHotelCommand;
+using CleanArc.Application.Features.SearchHotel.Commands.DeleteSearchHotelCommand;
+using CleanArc.Application.Features.SearchHotel.Commands.UpdateSearchHotelCommand;
+using CleanArc.Application.Features.SearchHotel.Queries.GetAllSearchHotels;
+using CleanArc.Application.Features.SearchHotel.Queries.GetSearchHotelById;
+using CleanArc.Application.Features.SearchHotelImage.Command.CreateSearchHotelImageCommand;
+using CleanArc.Application.Features.SearchHotelImage.Command.DeleteSearchHotelImageCommand;
+using CleanArc.Application.Features.SearchHotelImage.Command.UpdateSearchHotelImageCommand;
+using CleanArc.Application.Features.SearchHotelImage.Queries.GetAllSearchHotelImage;
+using CleanArc.Application.Features.SearchHotelImage.Queries.GetByIdSearchHotelImage;
+using CleanArc.WebFramework.BaseController;
+using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArc.Web.Api.Controllers.V1.SearchHotelImage;
+[ApiVersion("1")]
+[ApiController]
+[Route("api/v{version:apiVersion}/SearchHotelImage")]
+[Authorize]
+public class SearchHotelImageController : _BaseController<CreateSearchHotelImageCommand, UpdateSearchHotelImageCommand, DeleteSearchHotelImageCommand, bool, GetAllSearchHotelImageQuery,
+List<GetAllSearchHotelImageQueryResult>, GetByIdSearchHotelImageQuery, GetByIdSearchHotelImageQueryResult>
+{
+
+    public SearchHotelImageController(ISender sender, ILogger<_BaseController<CreateSearchHotelImageCommand, UpdateSearchHotelImageCommand, DeleteSearchHotelImageCommand, bool, GetAllSearchHotelImageQuery,
+List<GetAllSearchHotelImageQueryResult>, GetByIdSearchHotelImageQuery, GetByIdSearchHotelImageQueryResult>> logger, IHttpContextAccessor httpContextAccessor) : base(sender, logger, httpContextAccessor)
+    {
+
+    }
+
+}
---
+++ src/Core/CleanArc.Application/Features/SearchHotelImage/Queries/GetAllSearchHotelImage/GetAllSearchHotelImageQueryResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.SearchHotelImage.Queries.GetAllSearchHotelImage;
+
+public record GetAllSearchHotelImageQueryResult(int HotelID,string HotelName, string ImageTitle, string ImagePath, bool IsMain);
+
