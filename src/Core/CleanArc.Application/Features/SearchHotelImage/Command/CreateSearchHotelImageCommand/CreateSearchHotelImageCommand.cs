d0d52baf56faa21d4ecc7f8a70d9b22e6dd03e6b
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-06-04 16:32:36 +0500

    Searching Hotel

---
+++ src/Core/CleanArc.Application/Features/SearchHotelImage/Command/CreateSearchHotelImageCommand/CreateSearchHotelImageCommand.cs
@@ -0,0 +1,33 @@
+﻿using CleanArc.Application.Features.AgeType.Commands.CreateAgeTypeCommand;
+using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.ValidationBase.Contracts;
+using CleanArc.SharedKernel.ValidationBase;
+using FluentValidation;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace CleanArc.Application.Features.SearchHotelImage.Command.CreateSearchHotelImageCommand;
+
+public record CreateSearchHotelImageCommand(string? HotelName, string? ImageTitle, string? ImagePath, int? HotelID) : IRequest<OperationResult<bool>>,
+    IValidatableModel<CreateSearchHotelImageCommand>
+{
+    [JsonIgnore]
+    public int UserId { get; set; }
+    public IValidator<CreateSearchHotelImageCommand> ValidateApplicationModel(ApplicationBaseValidationModelProvider<CreateSearchHotelImageCommand> validator)
+    {
+        validator.RuleFor(c => c.HotelName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Please enter a valid Name");
+        validator.RuleFor(c => c.ImageTitle)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Please enter a Description");
+        return validator;
+    }
+}
---
+++ src/Core/CleanArc.Domain/Entities/SearchHotelAmenities/SearchHotelAmenities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Domain.Entities.SearchHotelAmenities
+{
+    public class SearchHotelAmenities
+    {
+        public string? Amenity { get; set; }
+        public string? Description { get; set; }
+    }
+}
