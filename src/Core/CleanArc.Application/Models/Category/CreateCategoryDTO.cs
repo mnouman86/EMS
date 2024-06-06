f628cce64beed6217fe90baf74146ac0b82c6de7
Waqas <aliw6065@gmail.com>
2024-06-06 16:48:24 +0500

    SeachRoomAmenity

--- src/Core/CleanArc.Application/Models/Category/CreateCategoryDTO.cs
+++ src/Core/CleanArc.Application/Models/Category/CreateCategoryDTO.cs
@@ -8,7 +8,7 @@ namespace CleanArc.Application.Models.Category
 {
     public class CreateCategoryDTO
     {
-        public int ID { get; set; }
+        //public int ID { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         //public bool? IsActive { get; set; }
