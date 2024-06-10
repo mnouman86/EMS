ea497cd835ba6cfcc76aa68bc9d42b90aa0f1dd6
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-06-10 16:52:14 +0500

    changes in getall_countries

--- src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/CountryQueries.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/CountryQueries.cs
@@ -11,7 +11,7 @@ namespace CleanArc.Infrastructure.Sql.SqlQueries
         public static string Create_Country => "Create_Country";
         public static string Delete_Country => "Delete_Country";
         public static string Update_Country => "Update_Country";
-        public static string usp_GetAll_CitytoCountryID => "usp_GetAll_CitytoCountryID";
+        public static string usp_GetALL_Country => "usp_GetALL_Country";
         public static string usp_GetByID_Country => "usp_GetByID_Country";
     }
 }
