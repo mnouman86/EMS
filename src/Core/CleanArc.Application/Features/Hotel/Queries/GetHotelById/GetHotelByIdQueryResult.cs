0b390250ed94b229655878a03a2a6a78f0c424d8
Waqas <aliw6065@gmail.com>
2024-06-11 18:58:44 +0500

    Minor changes in get All Hotel

--- src/Core/CleanArc.Application/Features/Hotel/Queries/GetHotelById/GetHotelByIdQueryResult.cs
+++ src/Core/CleanArc.Application/Features/Hotel/Queries/GetHotelById/GetHotelByIdQueryResult.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace CleanArc.Application.Features.Hotel.Queries.GetHotelById;
 
-public record GetHotelByIdQueryResult(int Id, string Name, int CountryID, int StateID, int CityID, int ZipCode, string Address1,
-string Address2, string Latitude, string Longitude, string MobileNumber, string PhoneNumber, string Email,
-string FocalPersonName, bool IsChanelManager, bool IsRating, bool IsChain, DateTime CheckInFrom, DateTime CheckInTo,
-DateTime CheckOutFrom, DateTime CheckOutTo, bool IsActive, bool IsDeleted, int CreatedBy, DateTime CreatedAt, DateTime UpdatedAt, int UpdatedBy);
+public class GetHotelByIdQueryResult
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CountryID { get; set; }
+    public int StateID { get; set; }
+    public int CityID { get; set; }
+    public int ZipCode { get; set; }
+    public string Address1 { get; set; }
+    public string Address2 { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public string MobileNumber { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string FocalPersonName { get; set; }
+    public bool IsChanelManager { get; set; }
+    public bool IsRating { get; set; }
+    public bool IsChain { get; set; }
+    public DateTime? CheckInFrom { get; set; }
+    public DateTime? CheckInTo { get; set; }
+    public DateTime? CheckOutFrom { get; set; }
+    public DateTime? CheckOutTo { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public int CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int UpdatedBy { get; set; }
+}
