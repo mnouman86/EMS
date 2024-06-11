1a440efcbd66b5477b3235754daae42551dabb94
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-06-11 16:30:29 +0500

    changes in CreateCity and ServiceCategory

--- src/Core/CleanArc.Application/Models/City/CreateCityDTO.cs
+++ src/Core/CleanArc.Application/Models/City/CreateCityDTO.cs
@@ -12,14 +12,14 @@ namespace CleanArc.Application.Models.City
         public string? Name { get; set; }
         public string? Description { get; set; }
         public int? StateID { get; set; }
-        public string? ImagePath { get; set; }
-        public string? ImageTitle { get; set; }
+        public string? ImagePath { get; set; } 
+        public string? ImageTitle { get; set; } 
        // public bool? IsActive { get; set; }
         //public bool? IsDeleted { get; set; }
         public int? CreatedBy { get; set; }
         //public DateTime? CreatedAt { get; set; }
         //public int? UpdatedBy { get; set; }
-       // public DateTime? UpdatedAt { get; set; }
-        public bool? IsMain { get; set; }
+        // public DateTime? UpdatedAt { get; set; }
+        public bool? IsMain { get; set; } 
     }
 }
