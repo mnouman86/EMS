0477982edb3f5bb3df791786c4995e7146139c69
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-07-09 10:51:26 +0500

    changes

--- src/UI/CleanArc.Web.UI/Controllers/HomeController.cs
+++ src/UI/CleanArc.Web.UI/Controllers/HomeController.cs
@@ -115,48 +115,7 @@ namespace CleanArc.Web.UI.Controllers
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
-        //[HttpGet]
-        //public async Task<IActionResult> GetImages(string category, string imagePath)
-        //{
-        //    try
-        //    {
-        //        // Check if imagePath is provided
-        //        if (string.IsNullOrWhiteSpace(imagePath))
-        //        {
-        //            return BadRequest(new { message = "Invalid image path." });
-        //        }
-
-        //        // Combine the base directory, category, and image path to construct the full path to the image file
-        //        var folderName = Path.Combine("Resources", "Images", category);
-        //        var pathToRetrieve = Path.Combine(Directory.GetCurrentDirectory(), folderName, imagePath);
-
-        //        // Check if the file exists at the constructed path
-        //        if (System.IO.File.Exists(pathToRetrieve))
-        //        {
-        //            // Read the file bytes
-        //            var imageData = await System.IO.File.ReadAllBytesAsync(pathToRetrieve);
-
-        //            // Get the content type based on the file extension
-        //            var contentType = GetImageContentType(imagePath);
-
-        //            // Return the file with the appropriate content type
-        //            return File(imageData, contentType);
-        //        }
-        //        else
-        //        {
-        //            // Return a 404 Not Found response if the file does not exist
-        //            return NotFound(new { message = "Image not found." });
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Log the error and return a 500 Internal Server Error response if an exception occurs
-        //        _logger.LogError($"Error retrieving image: {ex.Message}");
-        //        return StatusCode(500, $"Internal server error: {ex.Message}");
-        //    }
-        //}
-
-        // Helper method to determine the content type based on the file extension
+       
         private string GetImageContentType(string fileName)
         {
             var extension = Path.GetExtension(fileName).ToLowerInvariant();
@@ -169,30 +128,6 @@ namespace CleanArc.Web.UI.Controllers
                 _ => "application/octet-stream", // Default for unknown file types
             };
         }
-        //[HttpGet]
-        //public IActionResult GetImageByPath(string imagePath)
-        //{
-        //    try
-        //    {
-        //        if (System.IO.File.Exists(imagePath))
-        //        {
-        //            var imageData = System.IO.File.ReadAllBytes(imagePath);
-        //            var base64Image = Convert.ToBase64String(imageData);
-        //            var imageSrc = $"data:image/jpeg;base64,{base64Image}"; // Adjust content type based on your image type
-
-        //            return View("Index", imageSrc);
-        //        }
-        //        else
-        //        {
-        //            ViewBag.Message = "Image not found.";
-        //            return View("Index");
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError($"Error retrieving image: {ex.Message}");
-        //        return StatusCode(500, $"Internal server error: {ex.Message}");
-        //    }
-        //}
+       
     }
 }
