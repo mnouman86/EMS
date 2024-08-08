5ba42345669fb5939c0294843fd50f787b711aa6
Waqas <aliw6065@gmail.com>
2024-08-08 15:57:33 +0500

    ActivityGroup

---
+++ src/Core/CleanArc.Application/Features/ActivityGroup/Commands/DeleteActivityGroupCommand/DeleteActivityGroupCommand.cs
@@ -0,0 +1,33 @@
+﻿using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.ValidationBase.Contracts;
+using CleanArc.SharedKernel.ValidationBase;
+using FluentValidation;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace CleanArc.Application.Features.ActivityGroup.Commands.DeleteActivityGroupCommand;
+
+public record DeleteActivityGroupCommand(string SelectedIds, int? UpdatedBy, int? CultureId) : IRequest<OperationResult<bool>>,
+    IValidatableModel<DeleteActivityGroupCommand>
+{
+    [JsonIgnore]
+    public int UserId { get; set; }
+    public IValidator<DeleteActivityGroupCommand> ValidateApplicationModel(ApplicationBaseValidationModelProvider<DeleteActivityGroupCommand> validator)
+    {
+        validator.RuleFor(c => c.SelectedIds)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Please select Record");
+       
+        return validator;
+    }
+}
+
+//public record class DeleteActivityGroupCommand
+//{
+//}
---
+++ src/Core/CleanArc.Application/Models/ActivityGroup/UpdateActivityGroupDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Models.ActivityGroup
+{
+    public class UpdateActivityGroupDTO
+    {
+
+      
+        public int ID { get; set; }
+        public int? ActivityID { get; set; }
+        public string? From { get; set; } // Assumes the column name is "From"
+        public string? To { get; set; }
+        public int? Size { get; set; }
+        public int? UpdatedBy { get; set; }
+       // public DateTime? UpdatedAt { get; set; }
+        public int? CultureId { get; set; }
+        public int? Code { get; set; }
+        public int? Message { get; set; }
+    }
+}
