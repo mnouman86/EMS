465bc8e4b46f960511c26dc8bf7e8f3b395b90dd
Waqas <aliw6065@gmail.com>
2024-08-09 13:12:26 +0500

    Currency

---
+++ src/Core/CleanArc.Application/Contracts/Persistence/ICurrencyRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArc.Domain.Entities.Currency;
+using CleanArc.Domain.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Contracts.Persistence;
+
+public  interface ICurrencyRepository:IRepository<Currency>
+{
+   // Task CreateAgeType(AgeType ageType);
+}
---
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/CurrencyQueries.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Infrastructure.Sql.SqlQueries;
+
+public static class CurrencyQueries
+{
+    public static string Create_Currency => "Create_Currency";
+    public static string update_Currency => "update_Currency";
+    public static string Delete_Currency => "Delete_Currency";
+    public static string GetAll_Currency => "GetAll_Currency";
+    public static string GetByID_Currency => "GetByID_Currency";
+
+
+}
