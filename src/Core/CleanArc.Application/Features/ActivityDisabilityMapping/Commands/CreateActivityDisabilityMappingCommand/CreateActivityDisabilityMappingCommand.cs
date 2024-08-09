8c54803c328b4bb5e23655c6241419b0d50a2d41
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-08-09 15:04:51 +0500

    ActivityDisabilityMapping

---
+++ src/Core/CleanArc.Application/Features/ActivityDisabilityMapping/Commands/CreateActivityDisabilityMappingCommand/CreateActivityDisabilityMappingCommand.cs
@@ -0,0 +1,33 @@
+﻿using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.ValidationBase.Contracts;
+using CleanArc.SharedKernel.ValidationBase;
+using FluentValidation;
+using System.Text.Json.Serialization;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.ActivityDisabilityMapping.Commands.CreateActivityDisabilityMappingCommand;
+public record CreateActivityDisabilityMappingCommand( int? DisabilityOptionsLookupID, int? ActivityID, int? CreatedBy) : IRequest<OperationResult<bool>>,
+    IValidatableModel<CreateActivityDisabilityMappingCommand>
+{
+    [JsonIgnore]
+    public int UserId { get; set; }
+    public IValidator<CreateActivityDisabilityMappingCommand> ValidateApplicationModel(ApplicationBaseValidationModelProvider<CreateActivityDisabilityMappingCommand> validator)
+    {
+      
+        validator.RuleFor(c => c.DisabilityOptionsLookupID)
+    .NotEmpty()
+    .NotNull()
+    .WithMessage("Please enter a DisabilityOptionsLookupID");
+        validator.RuleFor(c => c.ActivityID)
+    .NotEmpty()
+    .NotNull()
+    .WithMessage("Please enter a ActivityID");
+        return validator;
+    }
+}
+
