fcd6439db7b2ea7133693a7a225b6f0ffbb59bc6
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-08-09 10:38:28 +0500

    changes

---
+++ src/Core/CleanArc.Application/Models/ActivitySeasonMapping - Copy/UpdateActivityDisabilityMappingDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Models.ActivityDisabilityMapping
+{
+    public class UpdateActivityDisabilityMappingDTO
+    {
+
+        public int ID { get; set; }
+        public int? DisabilityOptionsLookupID { get; set; }
+        public int? ActivityID { get; set; }
+        //public bool? IsActive { get; set; }
+        //public bool? IsDeleted { get; set; }
+        // public int? CreatedBy { get; set; }
+        //public DateTime? CreatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public int? CultureId { get; set; }
+        public int? Code { get; set; }
+        public int? Message { get; set; }
+        //public DateTime? UpdatedAt { get; set; }
+    }
+}
