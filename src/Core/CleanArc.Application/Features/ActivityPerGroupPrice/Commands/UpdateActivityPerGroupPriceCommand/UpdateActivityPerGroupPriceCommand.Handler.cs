1f4086202b71a2be42bacc44e7de520059f98184
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-08-12 16:41:04 +0500

    ActivityPerGroupPrice

---
+++ src/Core/CleanArc.Application/Features/ActivityPerGroupPrice/Commands/UpdateActivityPerGroupPriceCommand/UpdateActivityPerGroupPriceCommand.Handler.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using CleanArc.Application.Contracts.Identity;
+using CleanArc.Application.Contracts.Persistence;
+using CleanArc.Application.Features.ActivityPerGroupPrice.Commands.CreateActivityPerGroupPriceCommand;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediator;
+using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.Extensions;
+
+namespace CleanArc.Application.Features.ActivityPerGroupPrice.Commands.UpdateActivityPerGroupPriceCommand;
+
+internal class UpdateActivityPerGroupPriceCommandHandler:IRequestHandler<UpdateActivityPerGroupPriceCommand, OperationResult<bool>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IAppUserManager _userManager;
+    private readonly IConfiguration configuration;
+    private readonly IMapper _mapper;
+    private readonly ILogger<UpdateActivityPerGroupPriceCommandHandler> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor; // Add IHttpContextAccessor
+    //private readonly IUnitOfWork _unitOfWork;
+    //private readonly IAppUserManager _userManager;
+
+
+    public UpdateActivityPerGroupPriceCommandHandler(IUnitOfWork unitOfWork, IAppUserManager userManager, IConfiguration configuration, IMapper mapper, ILogger<UpdateActivityPerGroupPriceCommandHandler> logger, IHttpContextAccessor httpContextAccessor)
+    {
+        _unitOfWork = unitOfWork;
+        _userManager = userManager;
+        this.configuration = configuration;
+        _mapper = mapper;
+        _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+        //_unitOfWork = unitOfWork;
+        //_userManager = userManager;
+    }
+    public async ValueTask<OperationResult<bool>> Handle(UpdateActivityPerGroupPriceCommand request, CancellationToken cancellationToken)
+    {
+        using (var logger = _logger.LogMethodEntryExit(_httpContextAccessor?.HttpContext, request))
+        {
+
+            var user = await _userManager.GetUserByIdAsync(request.UserId);
+            if (user == null)
+                return OperationResult<bool>.FailureResult("User Not Found");
+
+            //await _unitOfWork.URLRepository.AddAsync(new Domain.Entities.UserManagement.URL()
+            //{ CreatedBy = user.Id, Path = request.Path, Title = request.Title, Description = request.Description/*, CreatedTime=DateTime.Now*/ });
+
+            //await _unitOfWork.CommitAsync();
+            //(logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(true);
+
+            //return OperationResult<bool>.SuccessResult(true);
+            await _unitOfWork.ActivityPerGroupPriceRepository.UpdateAsync(new Domain.Entities.ActivityPerGroupPrice.ActivityPerGroupPrice()
+            { UpdatedBy = user.Id,ID= request.ID, ActivityID = request.ActivityID, MinGroupSize = request.MinGroupSize, MaxGroupSize = request.MaxGroupSize, Price = request.Price });
+            await _unitOfWork.CommitAsync();
+            (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(true);
+            return OperationResult<bool>.SuccessResult(true);
+        }
+    }
+
+}
