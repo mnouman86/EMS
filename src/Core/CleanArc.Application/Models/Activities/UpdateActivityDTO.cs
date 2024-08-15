87233bcd92a511699d855a7061d28ad838dc1454
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-08-15 12:25:25 +0500

    changes in UpdateActivity

--- src/Core/CleanArc.Application/Models/Activities/UpdateActivityDTO.cs
+++ src/Core/CleanArc.Application/Models/Activities/UpdateActivityDTO.cs
@@ -10,40 +10,40 @@ namespace CleanArc.Application.Models.Activities
     {
 
         public int ID { get; set; }
+        public int? CultureId { get; set; }
         public string Title { get; set; }
         public int? LanguageLookUpID { get; set; }
         public int? ServiceLookUpID { get; set; }
         public int? SubServiceLookUpID { get; set; }
+        public string? OtherSubService { get; set; }
+
         public int? MinAge { get; set; }
         public int? MaxAge { get; set; }
         public int? ActivityTypeLookUpID { get; set; }
         public int? ActivityNatureLookUpID { get; set; }
-        public int? MinGroupSize { get; set; }
+        //public int? MinGroupSize { get; set; }
         public int? MaxGroupSize { get; set; }
-        public bool? IsPrivateActivity { get; set; }
-        public int? PrivateParticipantLookUpID { get; set; }
+        public int? IsPrivateActivity { get; set; }
         public string WhoCanParticipate { get; set; }
         public string WhoCannotParticipate { get; set; }
         public int? ManageActivityLookUpID { get; set; }
+        public string? OtherManageActivity { get; set; }
+
         public int? Days { get; set; }
         public int? Hours { get; set; }
         public string Description { get; set; }
         public bool? IsTransportation { get; set; }
         public int? TransportationLookUpID { get; set; }
         public bool? IsDisability { get; set; }
-        public int? Code { get; set; }
-        public int? Message { get; set; }
-        public int? ActivityID { get; set; }
         public string AllowedItems { get; set; }
         public string NotAllowedItems { get; set; }
         public int? CurrencyLookUpID { get; set; }
-        public int? SeasonLookUpID { get; set; }
-
-        public int? CultureId { get; set; }
-        public string? OtherSubService { get; set; }
-        public string? OtherManageActivity { get; set; }
         public Decimal? PerPersonPrice { get; set; }
-       // public int? PerGroupPrice { get; set; }
+        public string? SeasonLookUpID { get; set; }
+        public string? IncludeOptionLookUpID { get; set; }
+        public string? DisabilityOptionLookUpID { get; set; }
+        public int? Code { get; set; }
+        public int? Message { get; set; }
         public int? UpdatedBy { get; set; }
         //public DateTime? UpdatedAt { get; set; }
     }
