3504f1bdaa368939604930d758391fc6c09f8534
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-09-05 16:40:36 +0500

    UpdatedCarRentalSearchFilter

--- src/Core/CleanArc.Application/Models/Request/CarRentalSearchFilterRequest.cs
+++ src/Core/CleanArc.Application/Models/Request/CarRentalSearchFilterRequest.cs
@@ -8,14 +8,13 @@ namespace CleanArc.Application.Models.Request
 {
     public class CarRentalSearchFilterRequest : SearchRequest
     {
-        public string HotelName { get; set; } // NVARCHAR(MAX), NULL by default
-        public decimal? MaxPrice { get; set; } // DECIMAL(18,2), NULL by default
-        public decimal? MinPrice { get; set; } // DECIMAL(18,2), NULL by default
-        public string HotelAmenities { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string RoomAmenities { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string BathroomAmenities { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string RoomView { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string RoomFeature { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
+        public string? BusinessName { get; set; }
+        public string? CityName { get; set; }
+        public string? CarModel { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public decimal? MinPrice { get; set; }
+        public string CarAmenities { get; set; }
+
     }
-    
+
 }
--- src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/CarRentalSearchFilterQueries.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/CarRentalSearchFilterQueries.cs
@@ -11,7 +11,7 @@ namespace CleanArc.Infrastructure.Sql.SqlQueries
         //public static string Create_AgeType => "Create_AgeType";
         //public static string update_AgeType => "update_AgeType";
         //public static string Delete_AgeType => "Delete_AgeType";
-        public static string usp_GetALLByBusinessID_Cars => "usp_GetALLByBusinessID_Cars";
+        public static string GetAll_CarSearchFilter => "GetAll_CarSearchFilter";
         //public static string usp_GetByID_AgeType => "usp_GetByID_AgeType";
     }
 }
