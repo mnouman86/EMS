9b1c28468d697f9732881238b867d1c4fa34dbf1
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-09-10 16:04:21 +0500

    KBWhenToVisit

---
+++ src/Core/CleanArc.Application/Features/KBWhenToVisit/Queries/GetKBWhenToVisitById/GetKBWhenToVisitByIdQueryResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.KBWhenToVisit.Queries.GetKBWhenToVisitById
+{
+    public class GetKBWhenToVisitByIdQueryResult
+    //(int Id, string Name, string Description, bool IsDeleted, bool IsActive, int CreatedBy, DateTime CreatedAt, int UpdatedBy, DateTime UpdatedAt);
+    {
+        public int ID { get; set; }
+        //public int? KBDetailID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //public string URL { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int? CultureId { get; set; }
+        public int? Code { get; set; }
+        public int? Message { get; set; }
+    }
+}
