bc631f97e190963ff8b7821131e8b001a63d9784
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-09-10 11:37:12 +0500

    changes in amenity  query result

--- src/Core/CleanArc.Application/Features/Amenities/Queries/GetAmenitiesById/GetAmenityByIdQueryResult.cs
+++ src/Core/CleanArc.Application/Features/Amenities/Queries/GetAmenitiesById/GetAmenityByIdQueryResult.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace CleanArc.Application.Features.Amenities.Queries.GetAmenitiesById
 {
-    public class GetAmenityByIdQueryResult
-        //(int Id, string Name, string Description, int CategoryID, bool IsActive, bool IsDeleted,
-    //int CreatedBy, DateTime CreatedAt, int UpdatedBy, DateTime UpdatedAt);
-    {
+	public class GetAmenityByIdQueryResult
+	//(int Id, string Name, string Description, int CategoryID, bool IsActive, bool IsDeleted,
+	//int CreatedBy, DateTime CreatedAt, int UpdatedBy, DateTime UpdatedAt);
+	{
 
-    public int ID { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int CategoryID { get; set; }
-        public int? ServiceCategoryID { get; set; }
-        public string? ServiceCategoryName { get; set; }
-        public string Icon { get; set; }
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int CategoryID { get; set; }
+		public int? ServiceCategoryID { get; set; }
+		public string? ServiceCategoryName { get; set; }
+		public string Icon { get; set; }
 
-        public bool IsActive { get; set; }
-    public bool IsDeleted { get; set; }
-    public int CreatedBy { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public int UpdatedBy { get; set; }
-    public DateTime UpdatedAt { get; set; }
+		public bool IsActive { get; set; }
+		public bool IsDeleted { get; set; }
+		public int CreatedBy { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public int UpdatedBy { get; set; }
+		public DateTime UpdatedAt { get; set; }
 
-}
+	}
 
 }
--- src/Core/CleanArc.Application/Features/SearchFilterStay/Queries/GetAllSearchFilterStay/GetAllSearchFilterStayQuery.cs
+++ src/Core/CleanArc.Application/Features/SearchFilterStay/Queries/GetAllSearchFilterStay/GetAllSearchFilterStayQuery.cs
@@ -9,5 +9,5 @@ using System.Threading.Tasks;
 
 namespace CleanArc.Application.Features.SearchFilterStay.Queries.GetAllSearchFilterStay;
 
-public record GetAllSearchFilterStayQuery(SearchRequestStays searchRequestStays) : IRequest<OperationResult<List<GetAllSearchFilterStayQueryResult>>>;
+public record GetAllSearchFilterStayQuery(SearchRequestStays searchRequest) : IRequest<OperationResult<List<GetAllSearchFilterStayQueryResult>>>;
 
