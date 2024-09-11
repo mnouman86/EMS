606e03b5eec880f88cef66f092078c2b8a3ee311
Waqas <aliw6065@gmail.com>
2024-09-11 14:47:33 +0500

    Changes in GetAll_KBDescription

--- src/Core/CleanArc.Application/Features/KBDescription/Queries/GetAllKBDescription/GetAllKBDescriptionQueryResult.cs
+++ src/Core/CleanArc.Application/Features/KBDescription/Queries/GetAllKBDescription/GetAllKBDescriptionQueryResult.cs
@@ -9,10 +9,15 @@ namespace CleanArc.Application.Features.KBDescription.Queries.GetAllKBDescriptio
 public class GetAllKBDescriptionQueryResult
 {
     public int ID { get; set; }
+    public int? MediaID { get; set; }
     public int? KBDetailID { get; set; }
     public string? SubHeading { get; set; }
     public string? Content { get; set; }
     public string? KBContentType { get; set; }
+    public string? MediaType { get; set; }
+    public string? ImagePath { get; set; }
+    public string? ImageTitle { get; set; }
+    public bool? IsMain { get; set; }
     public bool? IsActive { get; set; }
     public bool? IsDeleted { get; set; }
     public int? CreatedBy { get; set; }
