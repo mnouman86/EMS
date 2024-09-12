6e57a612d671debf92addb86201a6b05975a938f
Waqas <aliw6065@gmail.com>
2024-09-12 11:57:04 +0500

    Chabges in KBDetail

--- src/Core/CleanArc.Application/Models/Request/ThingsToDoSearchFilterRequest.cs
+++ src/Core/CleanArc.Application/Models/Request/ThingsToDoSearchFilterRequest.cs
@@ -8,14 +8,12 @@ namespace CleanArc.Application.Models.Request
 {
     public class ThingsToDoSearchFilterRequest: SearchRequest
     {
-        public string HotelName { get; set; } // NVARCHAR(MAX), NULL by default
+        public string? ActivityName { get; set; }  // Nullable, NVARCHAR(MAX)
+        public string? CityName { get; set; }      // Nullable, NVARCHAR(MAX)
         public decimal? MaxPrice { get; set; } // DECIMAL(18,2), NULL by default
         public decimal? MinPrice { get; set; } // DECIMAL(18,2), NULL by default
-        public string HotelAmenities { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string RoomAmenities { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string BathroomAmenities { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string RoomView { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
-        public string RoomFeature { get; set; } // NVARCHAR(MAX), Comma-separated IDs, NULL by default
+        public string? ActivityCategory { get; set; }  // Comma-separated IDs, NVARCHAR(MAX)
+        public string? SeasonName { get; set; }
     }
     
 }
--- src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/SearchFilterThingsToDoQueries.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/SearchFilterThingsToDoQueries.cs
@@ -11,7 +11,7 @@ public static class SearchFilterThingsToDoQueries
     public static string Create_SearchFilterThingsToDo => "Create_SearchFilterThingsToDo";
     public static string update_SearchFilterThingsToDo => "update_SearchFilterThingsToDo";
     public static string Delete_SearchFilterThingsToDo => "Delete_SearchFilterThingsToDo";
-    public static string GetAll_SearchFilterThingsToDo => "GetAll_SearchFilterThingsToDo";
+    public static string GetAll_SearchFilterThingsToDo => "GetAll_ThingsToDoSearchFilter";
     public static string GetByID_SearchFilterThingsToDo => "GetByID_SearchFilterThingsToDo";
 
 
