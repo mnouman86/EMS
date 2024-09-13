e8cdef4a6a9a44fda9f02e7650e44a8be6b1c4d2
M Nouman Rafique <noumanrafique1986@gmail.com>
2024-09-13 09:27:04 +0500

    KBInterested

---
+++ src/Core/CleanArc.Application/Features/KBInterested/Commands/CreateKBInterestedCommand/CreateKBInterestedCommand.Handler.cs
@@ -0,0 +1,67 @@
+﻿using CleanArc.Application.Contracts.Identity;
+using CleanArc.Application.Contracts.Persistence;
+using CleanArc.Application.Features.URL.Commands.AddURLCommand;
+using CleanArc.Application.Models.Common;
+using CleanArc.Domain.Entities.User;
+using CleanArc.SharedKernel.Extensions;
+using MapsterMapper;
+using Mediator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.KBInterested.Commands.CreateKBInterestedCommand;
+
+internal class CreateKBInterestedCommandHandler: IRequestHandler<CreateKBInterestedCommand, OperationResult<bool>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IAppUserManager _userManager;
+    private readonly IConfiguration configuration;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CreateKBInterestedCommandHandler> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor; // Add IHttpContextAccessor
+    //private readonly IUnitOfWork _unitOfWork;
+    //private readonly IAppUserManager _userManager;
+
+
+    public CreateKBInterestedCommandHandler(IUnitOfWork unitOfWork, IAppUserManager userManager, IConfiguration configuration, IMapper mapper, ILogger<CreateKBInterestedCommandHandler> logger, IHttpContextAccessor httpContextAccessor)
+    {
+        _unitOfWork = unitOfWork;
+        _userManager = userManager;
+        this.configuration = configuration;
+        _mapper = mapper;
+        _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+        //_unitOfWork = unitOfWork;
+        //_userManager = userManager;
+    }
+
+    public async ValueTask<OperationResult<bool>> Handle(CreateKBInterestedCommand request, CancellationToken cancellationToken)
+    {
+        using (var logger = _logger.LogMethodEntryExit(_httpContextAccessor?.HttpContext, request))
+        {
+
+            var user = await _userManager.GetUserByIdAsync(request.UserId);
+            if (user == null)
+                return OperationResult<bool>.FailureResult("User Not Found");
+
+            //await _unitOfWork.URLRepository.AddAsync(new Domain.Entities.UserManagement.URL()
+            //{ CreatedBy = user.Id, Path = request.Path, Title = request.Title, Description = request.Description/*, CreatedTime=DateTime.Now*/ });
+
+            //await _unitOfWork.CommitAsync();
+            //(logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(true);
+
+            //return OperationResult<bool>.SuccessResult(true);
+            await _unitOfWork.KBInterestedRepository.AddAsync(new Domain.Entities.KBInterested.KBInterested()
+            { CreatedBy = user.Id,Name=request.Name,  Description = request.Description,Type=request.Type, CultureId =request.CultureId  });
+            await _unitOfWork.CommitAsync();
+            (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(true);
+            return OperationResult<bool>.SuccessResult(true);
+        }
+    }
+}
---
+++ src/Core/CleanArc.Application/Features/KBInterested/Commands/DeleteKBInterestedCommand/DeleteKBInterestedCommand.cs
@@ -0,0 +1,33 @@
+﻿using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.ValidationBase.Contracts;
+using CleanArc.SharedKernel.ValidationBase;
+using FluentValidation;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace CleanArc.Application.Features.KBInterested.Commands.DeleteKBInterestedCommand;
+
+public record DeleteKBInterestedCommand(string SelectedIds, int? UpdatedBy, int? CultureId) : IRequest<OperationResult<bool>>,
+    IValidatableModel<DeleteKBInterestedCommand>
+{
+    [JsonIgnore]
+    public int UserId { get; set; }
+    public IValidator<DeleteKBInterestedCommand> ValidateApplicationModel(ApplicationBaseValidationModelProvider<DeleteKBInterestedCommand> validator)
+    {
+        validator.RuleFor(c => c.SelectedIds)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Please select Record");
+       
+        return validator;
+    }
+}
+
+//public record class DeleteKBInterestedCommand
+//{
+//}
