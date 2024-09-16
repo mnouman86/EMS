6d4f1b552aceae082d7c417440d28112b4a38ce9
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2024-09-16 12:58:22 +0500

    changes in kb timing

--- src/Core/CleanArc.Domain/Entities/KBTiming/KBTiming.cs
+++ src/Core/CleanArc.Domain/Entities/KBTiming/KBTiming.cs
@@ -12,7 +12,7 @@ public  class KBTiming
 {
     public int ID { get; set; }
     public int? KBDetailID { get; set; }
-    public int? Day { get; set; }
+    public string? Day { get; set; }
     public string? TimeFrom { get; set; }
     public string? TimeTo { get; set; }
     public bool? IsAlwaysOpen { get; set; }
