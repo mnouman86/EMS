955028a4d1bf7f3866c92559bda813a290447197
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2024-10-09 10:22:32 +0500

    changes in parameters of  search room amenities respo

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/SearchRoomAmenitiesRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/SearchRoomAmenitiesRepository.cs
@@ -72,18 +72,29 @@ public class SearchRoomAmenitiesRepository : ISearchRoomAmenitiesRepository
             using (IDbConnection connection = new SqlConnection(configuration.GetConnectionString("DBConnection1")))
             {
                 connection.Open();
-                var parameters = new
-                {
-                    PageNumber = searchRequest.PageNumber,
-                    PageSize = searchRequest.PageSize,
-                    //SortingColumnName = searchRequest.SortingArray?.FirstOrDefault()?.SortingColumnName,
-                    //SortingColumnDirection = searchRequest.SortingArray?.FirstOrDefault()?.SortingColumnDirection,
-                    //FilterParameterName = searchRequest.FilterArray?.FirstOrDefault()?.ParameterName,
-                    //FilterParameterValue = searchRequest.FilterArray?.FirstOrDefault()?.ParameterValue
-                    SortingArray = DataTableHelper.ToDataTable(searchRequest.SortingArray), // Convert list to DataTable
-                    FilterArray = DataTableHelper.ToDataTable(searchRequest.FilterArray) // Convert list to DataTable
-                };
-                var result = await connection.QueryAsync<SearchRoomAmenities>(SearchRoomAmenitiesQueries.usp_GetByHotelID_RoomAmenities, parameters, commandType: CommandType.StoredProcedure);
+				//var parameters = new
+				//{
+				//    PageNumber = searchRequest.PageNumber,
+				//    PageSize = searchRequest.PageSize,
+				//    //SortingColumnName = searchRequest.SortingArray?.FirstOrDefault()?.SortingColumnName,
+				//    //SortingColumnDirection = searchRequest.SortingArray?.FirstOrDefault()?.SortingColumnDirection,
+				//    //FilterParameterName = searchRequest.FilterArray?.FirstOrDefault()?.ParameterName,
+				//    //FilterParameterValue = searchRequest.FilterArray?.FirstOrDefault()?.ParameterValue
+				//    SortingArray = DataTableHelper.ToDataTable(searchRequest.SortingArray), // Convert list to DataTable
+				//    FilterArray = DataTableHelper.ToDataTable(searchRequest.FilterArray) // Convert list to DataTable
+				//};
+
+				var parameters = new DynamicParameters();
+				parameters.Add("@PageNumber", searchRequest.PageNumber, DbType.Int32);
+				parameters.Add("@PageSize", searchRequest.PageSize, DbType.Int32);
+				parameters.Add("@cultureId", searchRequest.CultureId, DbType.Int32);
+				parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
+				parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
+				parameters.Add("@Code", dbType: DbType.Int32, direction: ParameterDirection.Output);
+				parameters.Add("@Message", dbType: DbType.String, size: 500, direction: ParameterDirection.Output);
+
+
+				var result = await connection.QueryAsync<SearchRoomAmenities>(SearchRoomAmenitiesQueries.usp_GetByHotelID_RoomAmenities, parameters, commandType: CommandType.StoredProcedure);
                 (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(result);
                 return result.ToList();
             }
