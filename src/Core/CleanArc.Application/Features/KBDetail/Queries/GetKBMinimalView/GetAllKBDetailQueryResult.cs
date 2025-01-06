f192f029b07b20e3c37643ca1ecea119cf7e5184
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-01-06 11:15:42 +0500

    Implementation of API for Knowledgebase landing page

---
+++ src/Core/CleanArc.Application/Features/KBDetail/Queries/GetKBMinimalView/GetAllKBDetailQueryResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.KBDetail.Queries.GetKBMinimalView;
+
+public class GetKBMinimalViewQueryResult
+{
+    public int? KBDetailID { get; set; }
+    public int? GenericTitleID { get; set; }
+    public int? KBDescriptionID { get; set; }
+    public string? Title { get; set; }
+    public int? SectionID { get; set; }
+    public string? Section { get; set; }
+    public string? Content { get; set; }
+    public int? ServiceID { get; set; }
+    public string? ServiceCategory { get; set; }
+    public int? CoreAreaLookupID { get; set; }
+    public string? CoreArea { get; set; }
+    public string? RelatedAreasLookupIDs { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+    public string? ImageTitle { get; set; }
+    public string? ImagePath { get; set; }
+}
---
+++ src/Core/CleanArc.Application/Features/KBDetail/Queries/GetKBMinimalView/GetKBMinimalViewQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using CleanArc.Application.Contracts.Persistence;
+using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.Extensions;
+using MapsterMapper;
+using Mediator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging; using CleanArc.Domain.Common;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.KBDetail.Queries.GetKBMinimalView
+{
+    internal class GetKBMinimalViewQueryHandler : IRequestHandler<GetKBMinimalViewQuery, OperationResult<List<GetKBMinimalViewQueryResult>>>
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<GetKBMinimalViewQueryHandler> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor; // Add IHttpContextAccessor
+
+
+        public GetKBMinimalViewQueryHandler(IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor, ILogger<GetKBMinimalViewQueryHandler> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            _logger = logger;
+
+        }
+
+        public async ValueTask<OperationResult<List<GetKBMinimalViewQueryResult>>> Handle(GetKBMinimalViewQuery request, CancellationToken cancellationToken)
+        {
+            using (var logger = _logger.LogMethodEntryExit(_httpContextAccessor?.HttpContext, request))
+            {
+                var Product = await _unitOfWork.KBDetailRepository.GetKBMinimalViewAsync(request.searchRequest);
+
+                //var resultCheck = uRLs.Select(c => new GetAllProductsQueryResult(c.Id, c.Path, c.Title, c.Description)).ToList();
+                var result = _mapper.Map<List<GetKBMinimalViewQueryResult>>(Product);
+                (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(result);
+                return OperationResult<List<GetKBMinimalViewQueryResult>>.SuccessResult(result);
+            }
+        }
+    }
+
+}
