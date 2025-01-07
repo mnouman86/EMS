9639aa46093aacc01c73039a7b27d62dd57b45a7
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-01-07 15:18:35 +0500

    Implementation of Core Area wise Knowledgebase minimal view

---
+++ src/Core/CleanArc.Domain/Entities/KBDetail/CoreAreas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Domain.Entities.KBDetail
+{
+    public class CoreAreas
+    {
+        public int? ID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Icon { get; set; }
+        public int? Type { get; set; }
+        public string? ImagePath { get; set; }
+        public List<KBMinimalDetail> KBMinimalDetails { get; set; }
+    }
+}
--- src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/KBDetailQueries.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/KBDetailQueries.cs
@@ -13,6 +13,7 @@ public static class KBDetailQueries
     public static string Delete_KBDetail => "Delete_KBDetail";
     public static string GetAll_KBDetail => "GetAll_KBDetail";
     public static string GetAll_KBMinimalDetail => "GetAll_KBMinimalDetail";
+    public static string GetAll_KBCoreAreasMinimalDetail => "GetAllCoreAreas_KBMinimalDetail";
     public static string GetByID_KBDetail => "GetByID_KBDetail";
 
 
