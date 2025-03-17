5f4e357118971d9a694333fbb731b1a916510171
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-03-17 11:24:59 +0500

    Addition of prefix usp_

--- src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/PopularItemsVisitQueries.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/PopularItemsVisitQueries.cs
@@ -8,11 +8,11 @@ namespace CleanArc.Infrastructure.Sql.SqlQueries;
 
 public static class PopularItemsVisitQueries
 {
-    public static string Create_PopularItemsVisit => "KnowledgeBase.Create_PopularItemsVisits";
-    public static string Update_PopularItemsVisit => "KnowledgeBase.Update_PopularItemsVisits";
-    public static string Delete_PopularItemsVisit => "KnowledgeBase.Delete_PopularItemsVisits";
-    public static string GetAll_PopularItemsVisit => "dbo.GetAll_PopularItemsVisits";
-    public static string GetByID_PopularItemsVisit => "KnowledgeBase.GetByID_PopularItemsVisits";
+    public static string Create_PopularItemsVisit => "KnowledgeBase.usp_Create_PopularItemsVisits";
+    public static string Update_PopularItemsVisit => "KnowledgeBase.usp_Update_PopularItemsVisits";
+    public static string Delete_PopularItemsVisit => "KnowledgeBase.usp_Delete_PopularItemsVisits";
+    public static string GetAll_PopularItemsVisit => "dbo.usp_GetAll_PopularItemsVisits";
+    public static string GetByID_PopularItemsVisit => "KnowledgeBase.usp_GetByID_PopularItemsVisits";
 
 
 }
