c748fcb228a6b532402a0ed046ebc7e3dea7892f
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-03-19 15:49:00 +0500

    Return Service Category Name in Get All call of Sub ServiceRepository

--- src/Core/CleanArc.Domain/Entities/SubService/SubService.cs
+++ src/Core/CleanArc.Domain/Entities/SubService/SubService.cs
@@ -12,6 +12,7 @@ public  class SubService
 {
     public int Id { get; set; }
     public int? ServiceCategoryId { get; set; }
+    public string? ServiceCategoryName { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public bool? IsActive { get; set; }
