2ca8e94b02cc41e13c057228ad1a01085bf00c0f
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-03-21 11:43:06 +0500

    Code fix

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/AdvertisementPlaceRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/AdvertisementPlaceRepository.cs
@@ -93,7 +93,8 @@ public async Task<ResponseEntity> AddAsync(AdvertisementPlace AdvertisementPlace
                 parameters.Add("@UpdatedBy", updatedBy);
                 parameters.Add("@CultureId", deleteRequest.CultureId);
                 var result = await connection.QueryFirstOrDefaultAsync<ResponseEntity>(AdvertisementPlaceQueries.Delete_Place, parameters, commandType: CommandType.StoredProcedure);
-                 (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(result); if (result != null) { result.Code = parameters.Get<int>("@Code"); result.Message = parameters.Get<string>("@Message"); }
+                 (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(result); 
+                //if (result != null) { result.Code = parameters.Get<int>("@Code"); result.Message = parameters.Get<string>("@Message"); }
                 return result;
             }
         }
