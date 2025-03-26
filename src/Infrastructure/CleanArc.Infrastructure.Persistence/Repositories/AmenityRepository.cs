d5f2e2d85fcdb5d19f48d3268754d46cfd760be3
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-03-26 11:47:27 +0500

    IsDeleted added in Amenity

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/AmenityRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/AmenityRepository.cs
@@ -89,6 +89,7 @@ public class AmenityRepository : IAmenityRepository
                 var parameters = new DynamicParameters();
                 parameters.Add("@Ids", deleteRequest.SelectedIds);
                 parameters.Add("@CultureId", deleteRequest.CultureId);
+                parameters.Add("@IsDeleted", deleteRequest.isDeleted);
                 parameters.Add("@UpdatedBy", updatedBy);
                 //parameters.Add("@Code", dbType: DbType.Int32, direction: ParameterDirection.Output);
                 //parameters.Add("@Message", dbType: DbType.String, size: 500, direction: ParameterDirection.Output);
