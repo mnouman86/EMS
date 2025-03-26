91c7c769c1cd6ceb62a14ddc6cf0896ad9fc467a
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-03-26 11:47:27 +0500

    IsDeleted added in Amenity

---
+++ src/Core/CleanArc.Domain/Enums/AmenityType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Domain.Enums
+{
+    public enum AmenityType
+    {
+        Stays = 1,
+        Car = 2,
+        ThingsToDo = 5,
+        Bathroom = 4,
+        Room = 3
+    }
+}
