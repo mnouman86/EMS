9127dee796bbf49b9d3701774f99769cd04f9ccf
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-03-27 14:01:43 +0500

    FAQs implementation

--- src/Core/CleanArc.Application/Features/FAQs/Commands/UpdateFAQsCommand/UpdateFAQsCommand.cs
+++ src/Core/CleanArc.Application/Features/FAQs/Commands/UpdateFAQsCommand/UpdateFAQsCommand.cs
@@ -11,33 +11,22 @@ using System.Threading.Tasks;
 using CleanArc.Application.Models.Request;using System.Text.Json.Serialization; using CleanArc.Domain.Common;
 
 namespace CleanArc.Application.Features.FAQs.Commands.UpdateFAQsCommand;
-public record UpdateFAQsCommand(int Id,int? CategoryServiceID, int? ServiceID, int? SubServiceID, string? Question, string? Answer, int? UpdatedBy, int? CultureId) : IRequest<OperationResult<ResponseEntity>>,
+public record UpdateFAQsCommand(int Id, string? Question,
+    string? Answer, int? CultureId) : IRequest<OperationResult<ResponseEntity>>,
     IValidatableModel<UpdateFAQsCommand>
 {
     [JsonIgnore]
     public int UserId { get; set; }
     public IValidator<UpdateFAQsCommand> ValidateApplicationModel(ApplicationBaseValidationModelProvider<UpdateFAQsCommand> validator)
     {
-        validator.RuleFor(c => c.CategoryServiceID)
-     .NotEmpty()
-     .NotNull()
-     .WithMessage("Please enter a valid CategoryServiceID");
-        validator.RuleFor(c => c.ServiceID)
-            .NotEmpty()
-            .NotNull()
-            .WithMessage("Please enter a ServiceID");
-        validator.RuleFor(c => c.SubServiceID)
-            .NotEmpty()
-            .NotNull()
-            .WithMessage("Please enter a SubServiceID");
         validator.RuleFor(c => c.Question)
             .NotEmpty()
             .NotNull()
-            .WithMessage("Please enter a Question");
+            .WithMessage("Please enter a Question.");
         validator.RuleFor(c => c.Answer)
     .NotEmpty()
     .NotNull()
-    .WithMessage("Please enter a QueAnswerstion");
+    .WithMessage("Please enter an Answer.");
         return validator;
     }
 }
--- src/Core/CleanArc.Application/Features/FAQs/Queries/GetFAQsById/GetFAQsByIdQueryResult.cs
+++ src/Core/CleanArc.Application/Features/FAQs/Queries/GetFAQsById/GetFAQsByIdQueryResult.cs
@@ -10,13 +10,9 @@ namespace CleanArc.Application.Features.FAQs.Queries.GetFAQsById
     //(int Id, string Name, string Description, bool IsDeleted, bool IsActive, int CreatedBy, DateTime CreatedAt, int UpdatedBy, DateTime UpdatedAt);
     {
         public int Id { get; set; }
-		public int CategoryServiceID { get; set; }
-		public int ServiceID { get; set; }
-		public int SubServiceID { get; set; }
-		public string ServiceName { get; set; }
+		public int GenericTitleId { get; set; }
 		public string Question { get; set; }
 		public string Answer { get; set; }
-		public string CategoryServiceName { get; set; }
 		public bool IsDeleted { get; set; }
         public bool IsActive { get; set; }
         public int CreatedBy { get; set; }
@@ -24,7 +20,5 @@ namespace CleanArc.Application.Features.FAQs.Queries.GetFAQsById
         public int UpdatedBy { get; set; }
         public DateTime UpdatedAt { get; set; }
         public int? CultureId { get; set; }
-        public int? Code { get; set; }
-        public string? Message { get; set; }
     }
 }
