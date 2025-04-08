d6faf44c1390fa1b14ba511a34067c5e1df42b11
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-04-08 17:36:58 +0500

    Activity Update fix

--- src/Core/CleanArc.Application/Features/Activity/Commands/UpdateActivityCommand/UpdateActivityCommand.Handler.cs
+++ src/Core/CleanArc.Application/Features/Activity/Commands/UpdateActivityCommand/UpdateActivityCommand.Handler.cs
@@ -59,7 +59,7 @@ internal class UpdateActivityCommandHandler:IRequestHandler<UpdateActivityComman
 
             //return OperationResult<ResponseEntity>.SuccessResult(result);
             var result=await _unitOfWork.ActivityRepository.UpdateAsync(new Domain.Entities.Activity.Activity()
-            { UpdatedBy = user.Id,GenericTitleId= request.Id,
+            { UpdatedBy = user.Id,Id= request.Id,
                 CultureId=request.CultureId,
                 BusinessId=request.BusinessId,
                 Title = request.Title,
