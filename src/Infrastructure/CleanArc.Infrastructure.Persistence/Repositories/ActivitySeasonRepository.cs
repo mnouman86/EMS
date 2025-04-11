77a8657ffe604f9928626455c071a7e86a389fc6
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-04-11 10:11:19 +0500

    Is deleted code fix

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/ActivitySeasonRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/ActivitySeasonRepository.cs
@@ -115,8 +115,8 @@ public async Task<ResponseEntity> AddAsync(ActivitySeason ActivitySeason)
                 parameters.Add("@PageNumber", searchRequest.PageNumber, DbType.Int32);
                 parameters.Add("@PageSize", searchRequest.PageSize, DbType.Int32);
                 parameters.Add("@cultureId", searchRequest.CultureId, DbType.Int32);
-                //parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
-                //parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
+                parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
+                parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
                 parameters.Add("@Code", dbType: DbType.Int32, direction: ParameterDirection.Output);
                 parameters.Add("@Message", dbType: DbType.String, size: 500, direction: ParameterDirection.Output);
 
