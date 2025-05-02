9850054735a8c96d56ebbb1e63df7fe3e9e33d37
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-02 14:03:08 +0500

    Name has been added

--- src/Core/CleanArc.Domain/Entities/Activity/Activity.cs
+++ src/Core/CleanArc.Domain/Entities/Activity/Activity.cs
@@ -23,6 +23,7 @@ public  class Activity
     public int Id { get; set; }
     public int? BusinessId { get; set; }
     public string Title { get; set; }
+    public string Name { get; set; }
     public int[]? LanguageLookUpId { get; set; }
     public List<LanguageLookUp>? Languages { get; set; }
     public List<IncludedOptionsLookup>? IncludedOptions { get; set; }
