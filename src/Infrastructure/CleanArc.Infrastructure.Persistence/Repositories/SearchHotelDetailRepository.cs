874077c9a5f8c0230a92d101171f25c520a66424
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-06 11:21:18 +0500

    Type added for filter

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/SearchHotelDetailRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/SearchHotelDetailRepository.cs
@@ -82,6 +82,7 @@ public class SearchHotelDetailRepository : ISearchHotelRepository
 				parameters.Add("@MaxPrice", searchRequest.MaxPrice, DbType.Int32);
 				parameters.Add("@Amenities", searchRequest.Amenities, DbType.String);
 				parameters.Add("@Name", searchRequest.Name, DbType.String);
+				parameters.Add("@Type", searchRequest.Type, DbType.String);
 				parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
 				parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
 				parameters.Add("@Code", dbType: DbType.Int32, direction: ParameterDirection.Output);
