d00b8441a1c2899dab0a6edf54752a22866e3286
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-14 11:17:13 +0500

    s removed from SPs against DisabilityOptions

--- src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/DisabilityOptionQueries.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Sql/SqlQueries/DisabilityOptionQueries.cs
@@ -8,11 +8,11 @@ namespace CleanArc.Infrastructure.Sql.SqlQueries;
 
 public static class DisabilityOptionQueries
 {
-    public static string Create_DisabilityOption => "usp_Create_DisabilityOptions";
-    public static string Update_DisabilityOption => "usp_Update_DisabilityOptions";
-    public static string Delete_DisabilityOption => "usp_Delete_DisabilityOptions";
-    public static string GetAll_DisabilityOption => "usp_GetAll_DisabilityOptions";
-    public static string GetByID_DisabilityOption => "usp_GetByID_DisabilityOptions";
+    public static string Create_DisabilityOption => "usp_Create_DisabilityOption";
+    public static string Update_DisabilityOption => "usp_Update_DisabilityOption";
+    public static string Delete_DisabilityOption => "usp_Delete_DisabilityOption";
+    public static string GetAll_DisabilityOption => "usp_GetAll_DisabilityOption";
+    public static string GetByID_DisabilityOption => "usp_GetByID_DisabilityOption";
 
 
 }
