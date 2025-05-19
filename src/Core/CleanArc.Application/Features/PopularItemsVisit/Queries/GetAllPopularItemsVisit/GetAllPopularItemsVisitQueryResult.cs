6b7712ab518649ce336526389ec5f517da347878
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-19 15:50:16 +0500

    rating review in popular items

--- src/Core/CleanArc.Application/Features/PopularItemsVisit/Queries/GetAllPopularItemsVisit/GetAllPopularItemsVisitQueryResult.cs
+++ src/Core/CleanArc.Application/Features/PopularItemsVisit/Queries/GetAllPopularItemsVisit/GetAllPopularItemsVisitQueryResult.cs
@@ -52,6 +52,8 @@ public class GetAllPopularItemsVisitQueryResult
     public string? PageURL { get; set; }
    // public string? VisitAt { get; set; }
     public int? TotalVisitCount { get; set; }
+    public int? ReviewsCount { get; set; }
+    public int? Rating { get; set; }
 }
 
 //    public GetAllPopularItemsVisitQueryResult(int iD, string name, string description, bool isDeleted, bool isActive, int createdBy, DateTime createdAt, int updatedBy, DateTime updatedAt)
