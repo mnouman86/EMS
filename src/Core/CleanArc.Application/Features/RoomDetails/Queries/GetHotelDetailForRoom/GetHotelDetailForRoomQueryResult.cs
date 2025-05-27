5e4e1b578402db7bcff8ace725c22b21ecdaf6e3
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-27 12:09:07 +0500

    Filter parameters added

--- src/Core/CleanArc.Application/Features/RoomDetails/Queries/GetHotelDetailForRoom/GetHotelDetailForRoomQueryResult.cs
+++ src/Core/CleanArc.Application/Features/RoomDetails/Queries/GetHotelDetailForRoom/GetHotelDetailForRoomQueryResult.cs
@@ -3,7 +3,9 @@ using CleanArc.Application.Features.CustomerReview.Queries.GetAllCustomerReviews
 using CleanArc.Application.Features.FAQs.Queries.GetAllFAQs;
 using CleanArc.Application.Features.GenericMedia.Queries.GetAllGenericMedia;
 using CleanArc.Application.Features.Hotel.Queries.GetHotelById;
+using CleanArc.Application.Features.OutDoor.Queries.GetAllOutDoor;
 using CleanArc.Application.Features.RoomDetails.Queries.GetAllRoomDetail;
+using CleanArc.Application.Features.RoomView.Queries.GetAllRoomView;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,5 +22,7 @@ namespace CleanArc.Application.Features.RoomDetails.Queries.GetHotelDetailForRoo
         public IEnumerable<GetAllAmenityMappingQueryResult> Amenities { get; set; }
         public IEnumerable<GetAllFAQsQueryResult> FAQs { get; set; }
         public IEnumerable<GetAllCustomerReviewsQueryResult> Reviews { get; set; }
+        public IEnumerable<GetAllRoomViewQueryResult> RoomView { get; set; }
+        public IEnumerable<GetAllOutDoorQueryResult> OutDoor { get; set; }
     }
 }
