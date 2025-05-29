6320f1a0b5e61447b8b75b850d3adde4c6a9bd1e
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-29 12:27:01 +0500

    Manufacturer CRUD

---
+++ src/Core/CleanArc.Application/Features/Manufacturer/Queries/GetAllManufacturer/GetAllManufacturerQueryHandler.cs
@@ -0,0 +1,72 @@
+﻿using CleanArc.Application.Contracts.Persistence;
+using CleanArc.Application.Features.AgeType.Queries.GetAllAgeType;
+using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.Extensions;
+using MapsterMapper;
+using Mediator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging; 
+using CleanArc.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArc.Application.Features.Activity.Queries.GetAllActivity;
+using CleanArc.Application.Features.Manufacturer.Queries.GetAllManufacturer;
+
+namespace CleanArc.Application.Features.Language.Queries.GetAllManufacturer;
+
+internal class GetAllManufacturerQueryHandler : IRequestHandler<GetAllManufacturerQuery, OperationResult<List<GetAllManufacturerQueryResult>>>
+{
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    private readonly ILogger<GetAllManufacturerQueryHandler> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor; // Add IHttpContextAccessor
+
+
+    public GetAllManufacturerQueryHandler(IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor, ILogger<GetAllManufacturerQueryHandler> logger)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
+
+    }
+
+    public async ValueTask<OperationResult<List<GetAllManufacturerQueryResult>>> Handle(GetAllManufacturerQuery request, CancellationToken cancellationToken)
+    {
+        using (var logger = _logger.LogMethodEntryExit(_httpContextAccessor?.HttpContext, request))
+        {
+            //var language = await _unitOfWork.LanguageRepository.GetAllAsync(request.searchRequest);
+
+            ////var resultCheck = uRLs.Select(c => new GetAllProductsQueryResult(c.Id, c.Path, c.Title, c.Description)).ToList();
+            //var result = _mapper.Map<List<GetAllManufacturerQueryResult>>(language);
+            //(logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(result);
+            //return OperationResult<List<GetAllManufacturerQueryResult>>.SuccessResult(result);
+
+            var response = await _unitOfWork.ManufacturerRepository.GetAllAsync(request.searchRequest);
+
+            if (response.Code != 200)
+            {
+                return OperationResult<List<GetAllManufacturerQueryResult>>.FailureResult(
+                    response.Message,
+                response.Code
+                );
+            }
+
+            var mappedResult = _mapper.Map<List<GetAllManufacturerQueryResult>>(response.Data);
+            (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(mappedResult);
+
+            return OperationResult<List<GetAllManufacturerQueryResult>>.SuccessResult(
+                mappedResult,
+                response.Code,
+                response.Message,
+                    response.TotalCount
+            );
+        }
+    }
+}
+
+
---
+++ src/Core/CleanArc.Application/Features/Manufacturer/Queries/GetAllManufacturer/GetAllManufacturerQueryResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Application.Features.Manufacturer.Queries.GetAllManufacturer
+{
+    public class GetAllManufacturerQueryResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int ManufacturerLookUpId { get; set; }
+
+    }
+}
