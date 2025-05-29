36f7705fcd8205b65633ede4a078ab04b2ca1887
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-05-29 10:47:47 +0500

    code fix

--- src/Core/CleanArc.Application/Models/Request/SearchRequest.cs
+++ src/Core/CleanArc.Application/Models/Request/SearchRequest.cs
@@ -47,6 +47,8 @@ namespace CleanArc.Application.Models.Request
         public string? Amenities { get; set; }
         public string? Name { get; set; }
         public string? Type { get; set; }
+        public string? RoomView { get; set; }
+        public string? OutDoor { get; set; }
     
     }
 
--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/SearchHotelDetailRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/SearchHotelDetailRepository.cs
@@ -84,8 +84,8 @@ public class SearchHotelDetailRepository : ISearchHotelRepository
 				parameters.Add("@Name", searchRequest.Name, DbType.String);
 				parameters.Add("@Type", searchRequest.Type, DbType.String);
 				parameters.Add("@PropertyType", searchRequest.Type, DbType.String);
-				parameters.Add("@RoomView", searchRequest.Type, DbType.String);
-				parameters.Add("@Outdoor", searchRequest.Type, DbType.String);
+				parameters.Add("@RoomView", searchRequest.RoomView, DbType.String);
+				parameters.Add("@OutDoor", searchRequest.OutDoor, DbType.String);
                 parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
 				parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
                 parameters.Add("@TotalCount", dbType: DbType.Int32, direction: ParameterDirection.Output);
