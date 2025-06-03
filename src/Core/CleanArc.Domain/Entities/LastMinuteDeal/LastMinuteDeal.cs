cfc2a78051bb44934913ebb79cdbb43d56d05995
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-06-03 16:02:44 +0500

    last minute deal

--- src/Core/CleanArc.Domain/Entities/LastMinuteDeal/LastMinuteDeal.cs
+++ src/Core/CleanArc.Domain/Entities/LastMinuteDeal/LastMinuteDeal.cs
@@ -23,5 +23,31 @@ namespace CleanArc.Domain.Entities.LastMinuteDeal
         public int? UpdatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
 		public int? CultureId { get; set; }
-	}
+        public int? NoOfRooms { get; set; }
+        public int? NoOfDays { get; set; }
+        public int? HotelID { get; set; }
+        public int? CityID { get; set; }
+        public String? Name { get; set; }
+        public String? CityName { get; set; }
+        public String? CityDescription { get; set; }
+        public String? RoomTypeName { get; set; }
+        public String? RoomTypeDescription { get; set; }
+        public Decimal? DiscountPercentage { get; set; }
+        public Decimal? DiscountAmount { get; set; }
+        public Decimal? DiscountedPrice { get; set; }
+        public String? ImagePath { get; set; }
+        public String? ImageTitle { get; set; }
+        public String? Description { get; set; }
+        public decimal? Longitude { get; set; }
+        public decimal? Latitude { get; set; }
+        public int? TotalDays { get; set; }
+        public Decimal? TotalPrice { get; set; }
+        public Decimal? RoomDetailPrice { get; set; }
+        public int? ReviewsCount { get; set; }
+        public int? Rating { get; set; }
+        public string? RefundPolicy { get; set; }
+        public List<GenericMedia.GenericMedia> HotelImages { get; set; }
+        public List<AmenityMapping.AmenityMapping> Amenities { get; set; }
+        public string? FilterCategoryName { get; set; }
+    }
 }
