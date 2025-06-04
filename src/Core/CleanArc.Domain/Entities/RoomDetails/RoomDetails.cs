065c52b5286d4980dfa475036740ac354a31547b
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-06-04 15:29:35 +0500

    Add isDealExist bit

--- src/Core/CleanArc.Domain/Entities/RoomDetails/RoomDetails.cs
+++ src/Core/CleanArc.Domain/Entities/RoomDetails/RoomDetails.cs
@@ -36,6 +36,7 @@ public class RoomDetails
     public decimal? AdditionalMatricCharges { get; set; }
     public string? RoomNumber { get; set; }
     public bool? IsAvailable { get; set; }
+    public bool? IsDealExist { get; set; }
     public bool? IsActive { get; set; }
     public bool? IsDeleted { get; set; }
     public int? CreatedBy { get; set; }
