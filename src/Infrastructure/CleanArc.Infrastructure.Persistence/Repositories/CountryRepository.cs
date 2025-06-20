d184f796d3abe33b39e719747f1759bd6dfbcad2
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-06-20 12:06:46 +0500

    -1 for no page size implementation in country get all

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/CountryRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/CountryRepository.cs
@@ -119,8 +119,9 @@ public class CountryRepository : ICountryRepository
 
 				var parameters = new DynamicParameters();
 				parameters.Add("@PageNumber", searchRequest.PageNumber, DbType.Int32);
-				parameters.Add("@PageSize", searchRequest.PageSize, DbType.Int32);
-				parameters.Add("@cultureId", searchRequest.CultureId, DbType.Int32);
+                if (searchRequest.PageSize > 0)
+                parameters.Add("@PageSize", searchRequest.PageSize, DbType.Int32);
+                parameters.Add("@cultureId", searchRequest.CultureId, DbType.Int32);
 				parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
 				parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type				
                 parameters.Add("@TotalCount", dbType: DbType.Int32, direction: ParameterDirection.Output);
