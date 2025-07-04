c73fbfd39463be502c63abf63e53f194f7bdf34a
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-07-04 16:36:19 +0500

    added UserId in Search Request

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/LastMinuteDealRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/LastMinuteDealRepository.cs
@@ -109,6 +109,7 @@ public class LastMinuteDealRepository : ILastMinuteDealRepository
 				var parameters = new DynamicParameters();
 				parameters.Add("@PageNumber", searchRequest.PageNumber, DbType.Int32);
 				if (searchRequest.PageSize > 0) parameters.Add("@PageSize", searchRequest.PageSize, DbType.Int32);
+				if (searchRequest.UserId!=null) parameters.Add("@UserId", searchRequest.UserId, DbType.Int32);
 				parameters.Add("@cultureId", searchRequest.CultureId, DbType.Int32);
 				parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
 				parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
