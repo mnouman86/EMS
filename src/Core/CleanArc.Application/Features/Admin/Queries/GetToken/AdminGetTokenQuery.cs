e7f474e560deb99da1963a185586745ab3975e40
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2025-07-21 12:17:08 +0500

    remove validation method from login

--- src/Core/CleanArc.Application/Features/Admin/Queries/GetToken/AdminGetTokenQuery.cs
+++ src/Core/CleanArc.Application/Features/Admin/Queries/GetToken/AdminGetTokenQuery.cs
@@ -16,15 +16,13 @@ public record AdminGetTokenQuery(
     public IValidator<AdminGetTokenQuery> ValidateApplicationModel(ApplicationBaseValidationModelProvider<AdminGetTokenQuery> validator)
     {
         // Username validation (alphanumeric with underscores/dots)
-        validator.RuleFor(c => c.UserName)
-            .ValidEmail();
-            //.NotEmpty().WithMessage("Username is required")
-            //.Length(3, 50).WithMessage("Username must be between 3 and 50 characters")
-            //.Matches(@"^[a-zA-Z0-9_.]+$").WithMessage("Username can only contain letters, numbers, underscores, or dots");
+        validator.RuleFor(c => c.UserName);
+        //.NotEmpty().WithMessage("Username is required")
+        //.Length(3, 50).WithMessage("Username must be between 3 and 50 characters")
+        //.Matches(@"^[a-zA-Z0-9_.]+$").WithMessage("Username can only contain letters, numbers, underscores, or dots");
 
         // Strong password validation
-        validator.RuleFor(c => c.Password)
-            .ValidPassword();
+        validator.RuleFor(c => c.Password);
 
         return validator;
     }
