52904f897145308988272a8372fc89d88b5428bc
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-07-29 14:58:43 +0500

    code fix

--- src/Core/CleanArc.Application/Models/ContactForm/UpdateContactFormDTO.cs
+++ src/Core/CleanArc.Application/Models/ContactForm/UpdateContactFormDTO.cs
@@ -10,10 +10,10 @@ namespace CleanArc.Application.Models.ContactForm
     {
 
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public int? Type { get; set; }
-        public string? ImagePath { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
         public int? CultureId { get; set; }
     }
 }
