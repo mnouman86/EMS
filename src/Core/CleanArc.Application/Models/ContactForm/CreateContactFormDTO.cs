cf75abf7ba975b0cfb7e233676edc240aea863b2
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-07-29 14:56:14 +0500

    DTO Updated

--- src/Core/CleanArc.Application/Models/ContactForm/CreateContactFormDTO.cs
+++ src/Core/CleanArc.Application/Models/ContactForm/CreateContactFormDTO.cs
@@ -13,11 +13,7 @@ namespace CleanArc.Application.Models.ContactForm
         public string? Description { get; set; }
         public int? Type { get; set; }
         public string? ImagePath { get; set; }
-
-        public int? CreatedBy { get; set; }
         public int? CultureId { get; set; }
-        public int? Code { get; set; }
-        public string? Message { get; set; }
         //public DateTime? CreatedAt { get; set; }
         //public int? UpdatedBy { get; set; }
         //public DateTime? UpdatedAt { get; set; }
