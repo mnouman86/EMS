7c4b6f566565cfe3024c7e42ceee3f4441bd2212
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-09-04 10:18:55 +0500

    Added SubServiceName

--- src/Core/CleanArc.Domain/Entities/Activity/Activity.cs
+++ src/Core/CleanArc.Domain/Entities/Activity/Activity.cs
@@ -42,6 +42,8 @@ public  class Activity
     public int? ServiceCategoryId { get; set; }
     public int? SubServiceCategoryId { get; set; }
     public string? OtherSubService { get; set; }
+    public string? SubServiceName { get; set; }
+
     public string? AgeGroup { get; set; }
     public int? MinAge { get; set; }
     public int? MaxAge { get; set; }
