dfd4783a6926b1f825b7ec0c1966cc8231ae8f4c
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-09-16 12:24:59 +0500

    Uncomment filter and sorting array for get all orders

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/ProcessOrderRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/ProcessOrderRepository.cs
@@ -239,8 +239,8 @@ public class ProcessOrderRepository:IProcessOrderRepository
                 parameters.Add("@PageNumber", searchRequest.PageNumber, DbType.Int32);
                 if (searchRequest.PageSize > 0) parameters.Add("@PageSize", searchRequest.PageSize, DbType.Int32);
                 parameters.Add("@cultureId", searchRequest.CultureId, DbType.Int32);
-                //parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
-                //parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
+                parameters.Add("@SortingArray", DataTableHelper.ToDataTable(searchRequest.SortingArray), DbType.Object); // Ensure proper type
+                parameters.Add("@FilterArray", DataTableHelper.ToDataTable(searchRequest.FilterArray), DbType.Object); // Ensure proper type
                 parameters.Add("@TotalCount", dbType: DbType.Int32, direction: ParameterDirection.Output);
                 parameters.Add("@Code", dbType: DbType.Int32, direction: ParameterDirection.Output);
                 parameters.Add("@Message", dbType: DbType.String, size: 500, direction: ParameterDirection.Output);
