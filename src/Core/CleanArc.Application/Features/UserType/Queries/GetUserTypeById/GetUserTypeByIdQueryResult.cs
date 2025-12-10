fbe58d87b3914bb91802af203bcd8ef2d741c438
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2025-12-10 12:58:44 +0500

    issue fix

--- src/Core/CleanArc.Application/Features/UserType/Queries/GetUserTypeById/GetUserTypeByIdQueryResult.cs
+++ src/Core/CleanArc.Application/Features/UserType/Queries/GetUserTypeById/GetUserTypeByIdQueryResult.cs
@@ -9,13 +9,18 @@ namespace CleanArc.Application.Features.UserType.Queries.GetUserTypeById;
 public class GetUserTypeByIdQueryResult
 //(int Id, string Name, string Description, bool IsDeleted, bool IsActive, int CreatedBy, DateTime CreatedAt, int UpdatedBy, DateTime UpdatedAt);
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public bool IsDeleted { get; set; }
-    public bool IsActive { get; set; }
-    public int CreatedBy { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public int UpdatedBy { get; set; }
-    public DateTime UpdatedAt { get; set; }
+	public int? Id { get; set; }
+	public string? Title { get; set; }
+	public string? Description { get; set; }
+
+	public int? NoOfBookings { get; set; }
+	public decimal? DiscountPercentage { get; set; }
+	public decimal? DiscountCap { get; set; }
+	public bool? IsActive { get; set; }
+	public bool? IsDeleted { get; set; }
+	public int? CreatedBy { get; set; }
+	public DateTime? CreatedAt { get; set; }
+	public int? UpdatedBy { get; set; }
+	public DateTime? UpdatedAt { get; set; }
+	public int? CultureId { get; set; }
 }
