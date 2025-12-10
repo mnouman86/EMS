49afb809b3f262d7f55213271fc57f462e07493b
Faisal Naeem <faisal.naeem@siddiqihospitality.com>
2025-12-10 15:07:33 +0500

    room types against hotel get record

---
+++ src/Core/CleanArc.Application/Features/RoomType/Queries/GetRoomTypeByHotelId/GetRoomTypeByHotelIdQueryHandler.cs
@@ -0,0 +1,74 @@
+﻿using CleanArc.Application.Contracts.Persistence;
+using CleanArc.Application.Models.Common;
+using CleanArc.SharedKernel.Extensions;
+using MapsterMapper;
+using Mediator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging; using CleanArc.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArc.Application.Features.RoomVisual.Queries.GetRoomVisualById;
+
+namespace CleanArc.Application.Features.RoomType.Queries.GetRoomTypeByHotelId;
+
+internal class GetRoomTypeByHotelIdQueryHandler : IRequestHandler<GetRoomTypeByHotelIdQuery, OperationResult<GetRoomTypeByHotelIdQueryResult>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<GetRoomTypeByHotelIdQueryHandler> _logger;
+    private readonly IMapper _mapper;
+    private readonly IHttpContextAccessor _httpContextAccessor; // Add IHttpContextAccessor
+
+
+
+    public GetRoomTypeByHotelIdQueryHandler(IUnitOfWork unitOfWork, ILogger<GetRoomTypeByHotelIdQueryHandler> logger, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
+    }
+    public async ValueTask<OperationResult<GetRoomTypeByHotelIdQueryResult>> Handle(GetRoomTypeByHotelIdQuery request, CancellationToken cancellationToken)
+    {
+        using (var logger = _logger.LogMethodEntryExit(_httpContextAccessor?.HttpContext, request))
+        {
+            //var roomType = await _unitOfWork.RoomTypeRepository.GetByIdAsync(request.searchRequestById);
+
+            //if (roomType == null)
+            //{
+            //    return OperationResult<GetRoomTypeByHotelIdQueryResult>.NotFoundResult("roomType not found");
+            //}
+
+            ////var result = new GetURLByIdQueryResult(url.Id, url.Path, url.Title, url.Description);
+            //var result = _mapper.Map<GetRoomTypeByHotelIdQueryResult>(roomType);
+
+            //(logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(result);
+
+            //return OperationResult<GetRoomTypeByHotelIdQueryResult>.SuccessResult(result);
+
+            var response = await _unitOfWork.RoomTypeRepository.GetByIdAsync(request.searchRequestById);
+
+            if (response.Code != 200)
+            {
+                return OperationResult<GetRoomTypeByHotelIdQueryResult>.FailureResult(
+                    response.Message,
+                response.Code
+                );
+            }
+
+            var mappedResult = _mapper.Map<GetRoomTypeByHotelIdQueryResult>(response.Data);
+            (logger as LoggingExtensions.MethodEntryExitLogger)?.SetResponse(mappedResult);
+
+            return OperationResult<GetRoomTypeByHotelIdQueryResult>.SuccessResult(
+                mappedResult,
+                response.Code,
+                response.Message
+            );
+        }
+    }
+
+   
+}
+
