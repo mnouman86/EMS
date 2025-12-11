480107c9b125b9138b839336ed061787461930a1
M Nouman Rafique <noumanrafique1986@gmail.com>
2025-12-11 11:38:59 +0500

    issue fix

--- src/Core/CleanArc.Domain/Entities/RoomDiscountByUserType/RoomDiscountByUserType.cs
+++ src/Core/CleanArc.Domain/Entities/RoomDiscountByUserType/RoomDiscountByUserType.cs
@@ -10,6 +10,10 @@ public class RoomDiscountByUserType
 {
 	public int? Id { get; set; }
 	public int? GenericTitleId { get; set; }
+	public string? HotelName { get; set; }
+	public string? RoomType { get; set; }
+	public string? RatePlanNames { get; set; }
+	public string? UserTypeNames { get; set; }
 	public int? RoomDetailId { get; set; }
 	public string? RatePlanTypeIds { get; set; }
 	public string? UserTypeIds { get; set; }
