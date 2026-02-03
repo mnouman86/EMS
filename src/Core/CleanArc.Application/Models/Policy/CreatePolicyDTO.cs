94b6c731bb75e3bd3f97b379b38173dfc53db474
M Nouman Rafique <noumanrafique1986@gmail.com>
2026-02-03 12:34:31 +0500

    Policy update

--- src/Core/CleanArc.Application/Models/Policy/CreatePolicyDTO.cs
+++ src/Core/CleanArc.Application/Models/Policy/CreatePolicyDTO.cs
@@ -8,6 +8,8 @@ namespace CleanArc.Application.Models.Policy
 {
     public class CreatePolicyDTO
     {
+        public int? GenericTitleId { get; set; }
+        public int? ServiceTypeEnumId { get; set; }
         // public int Id { get; set; }
         public int? RatePlanTypeID { get; set; }
         public string Description { get; set; }
--- src/Core/CleanArc.Application/Models/Policy/UpdatePolicyDTO.cs
+++ src/Core/CleanArc.Application/Models/Policy/UpdatePolicyDTO.cs
@@ -8,7 +8,9 @@ namespace CleanArc.Application.Models.Policy;
 
 public class UpdatePolicyDTO
 {
-     public int Id { get; set; }
+    public int? GenericTitleId { get; set; }
+    public int? ServiceTypeEnumId { get; set; }
+    public int Id { get; set; }
     public int? RatePlanTypeID { get; set; }
     public string Description { get; set; }
 
